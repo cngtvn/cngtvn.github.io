20ac42e93df08f1106151c8c283cb7092bd1ff5f
iGosu <ct.gossu@gmail.com>
2018-03-07 21:40:18 +0700

    init

--- packages.sh
+++ packages.sh
@@ -2,6 +2,8 @@ dpkg-deb -Zgzip -b 3dnodiv11
 dpkg-deb -Zgzip -b littlex
 dpkg-deb -Zgzip -b inApp
 dpkg-deb -Zgzip -b i8Corners
+dpkg-deb -Zgzip -b batterywidget
+dpkg-deb -Zgzip -b nocarriername
 
 
 
